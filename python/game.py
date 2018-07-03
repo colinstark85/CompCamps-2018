684a5a1604a073a03620000ba5214e827ef3382c
colin <colinstark85@gmail.com>
2018-07-03 09:59:16 -0600

    python

---
+++ python/game.py
@@ -0,0 +1,45 @@
+import random
+import sys
+
+name = input("who dare chalange the mits?")
+health= 40
+class MIT(object):
+    def __init__(self,name):
+        self.name =name
+        self.health= 10
+        self.damage= random.randint(1,5)
+
+def isAlive(self):
+    return self.health > 0
+
+def attack(self):
+    hitDamage = random.randint(0,10)
+    self.health -= hitDamage
+    return hitDamage
+
+mits = [
+ MIT  ("bennett"),
+ MIT ("kaitlin"),
+ MIT ("rhiamon"),
+ MIT ("austin"),
+ MIT ("travis")
+ ]
+
+random.shuffle(mits)
+score = 0
+while len(mits) > 0:
+    mit = mits.pop()
+    print("wild {} appears".format(mit.name))
+    while mit.isAlive():
+        print("do you want to fight or flee?")
+        if input("fight / flee > ").lower() == "fight":
+                damage = mit.attack()
+                print("you did {} damage".format(damage))
+        else:
+            caught = random.randint(1,5) == 1
+            if not caught:
+                print("you have escaped")
+                print("you score was {}".format(score))
+                sys.exit(0)
+            else:
+                print("you falied to run away")
---
+++ python/main.py
@@ -0,0 +1,12 @@
+name = input("hello what is your name?")
+
+def welcome(name):
+    if name == "colin":
+        print("hello colin")
+    else:
+        print ("welcome to compcamps 2018 {}".format(name))
+
+        welcome(name)
+
+        mits = ["bennett","kaitlin","rhiamon","austin","travis"]
+        for mit in mit:
